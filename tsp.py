f8c43810aaadbc1900f63bfd5bf7b5c249e50bab
Manu Gopinathan <manugopi92@gmail.com>
2016-10-29 15:18:27 +0200

    Implement finding closest neuron to city

--- tsp.py
+++ tsp.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 from parser import parse
 from matplotlib import pyplot as plt
 import math
-from operator import attrgetter
+import random
 
 DATA_WESTERN_SAHARA = 'data/wi29.tsp'
 DATA_DJIBOUTI = 'data/dj38.tsp'
@@ -32,12 +32,20 @@ def get_distance(city1, city2):
 
 
 def normalize_coordinates(city_data):
+    """
+    Scale coordinates to the range [0, 1], centered around 0.5
+    :param city_data:
+    :return:
+    """
     normalized_city_coordinates = {}
 
     min_x, max_x, min_y, max_y = get_max_and_min(city_data)
 
+    # Centering
     centering_offset_x = max_x - (max_x - min_x) / 2
     centering_offset_y = max_y - (max_y - min_y) / 2
+
+    # Scaling
     scale = max(max_x - min_x, max_y - min_y)
 
     for city in city_data:
@@ -47,6 +55,24 @@ def normalize_coordinates(city_data):
         )
 
     return normalized_city_coordinates
+
+
+def get_closest_neuron(neurons, city_position):
+    """
+    Find neuron closest to given city coordinates
+    :param neurons: list of neurons
+    :param city_position: (x,y) tuple
+    :return: neuron and distance
+    """
+
+    distances = []  # Distance to each neuron
+
+    for n in neurons:  # type: Neuron
+        distances.append(get_distance(city_position, (n.x_pos, n.y_pos)))
+
+    min_distance = min(distances)
+    bmu = neurons[distances.index(min_distance)]
+    return bmu, min_distance
 
 
 def plot(city_data, neuron_list=[], save=False, filename=''):
@@ -91,19 +117,25 @@ def get_max_and_min(city_data):
 
     return min_x, max_x, min_y, max_y
 
+def adapt_neuron_positions(neuron, learning_rate, neighborbood_radius):
+    pass
+
 
 def tsp_som(cities_datafile, number_of_iterations, learning_rate=0.1, neighborhood_radius=2):
-    city_positions = parse(cities_datafile)  # Dictionary containing tuples of (x,y) coords
-    number_of_neurons = len(city_positions)
-    norma_city_positions = normalize_coordinates(city_positions)
-    neurons = initialize_neurons(number_of_neurons, norma_city_positions)
+    # Dictionary containing tuples of (x,y) coordinates
+    # Key=numbering from 0 and up. Value = (x,y)
+    cities = parse(cities_datafile)
 
+    number_of_neurons = len(cities)
+    norm_cities = normalize_coordinates(cities)  # Dictionary containing normalized (x,y) coordinates
+    neurons = initialize_neurons(number_of_neurons, norm_cities)
 
-    # plot(city_positions, save=True, filename='ws_nonnormalized.png')
-    plot(norma_city_positions, neurons, save=False, filename='ws_normalized.png')
+    plot(norm_cities, neurons, save=False, filename='ws_normalized.png')
 
-    # for i in range(number_of_iterations):
-    #
+    for i in range(number_of_iterations):
+        chosen_city = random.choice(norm_cities.keys())
+        bmu = get_closest_neuron(neurons, cities[chosen_city])  # Best matching unit
+        adapt_neuron_positions(bmu, learning_rate, neighborhood_radius)
 
 
 tsp_som(DATA_WESTERN_SAHARA, 10)
@@ -112,3 +144,13 @@ tsp_som(DATA_WESTERN_SAHARA, 10)
 # city_data = normalize_coordinates(parse(DATA_URUGUAY))
 #
 # plot(city_data)
+
+
+
+#TEST get closest neuron
+# cities = {}
+# cities[0] = (1, 2)
+# neurons = [Neuron(0, 10, 10), Neuron(1, 4, 7), Neuron(2, 5, 5)]
+# closest, distance = get_closest_neuron(neurons, cities[0])
+# print(closest.id, closest.x_pos, closest.y_pos, distance)
+# plot(cities, neurons)
